447fa645a5c947b36c3a7b8a8c5a75a96bdec76b
Artem <artemmikaelian@mail.ru>
2022-10-31 01:20:30 +0300

    добавил логирование, сделал некоторые правки

--- src/main/java/main/rest/CustomerController.java
+++ src/main/java/main/rest/CustomerController.java
@@ -2,16 +2,20 @@ package main.rest;
 
 import main.Service.CustomerService;
 import main.model.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 public class CustomerController {
+    private static final Logger LOGGER = LoggerFactory.getLogger("default");
     @Autowired
     private CustomerService customerService;
 
@@ -34,6 +38,8 @@ public class CustomerController {
         if (result.hasErrors()) {
             return "add_customer";
         }
+
+        LOGGER.info("Добавлен новый клиент.");
         customerService.save(customer);
         return "new_customer";
     }
@@ -50,36 +56,43 @@ public class CustomerController {
 
     @PostMapping("/update")
     public String updateUser(@RequestParam int id, @Valid Customer customer, BindingResult result) {
-            if (result.hasErrors()) {
-                return "edit";
-            }
-            customerService.save(customer);
-            return "edited_customer";
+        if (result.hasErrors()) {
+            LOGGER.info("Инфомрация о клиенте с ID=" + id + "не может быть обновлена, форма заполнена некорректно");
+            return "edit";
+        }
+        customerService.save(customer);
+        LOGGER.info("Информация о клиенте с ID=" + id + " была обновлена");
+        return "edited_customer";
     }
 
     @GetMapping("/delete")
     public String deleteUser(@RequestParam int id) {
         if (customerService.findById(id).isPresent()) {
+            LOGGER.info("Клиент с ID=" + id + " удален");
             customerService.delete(customerService.findById(id).get());
             return "redirect:/";
         } else {
+            LOGGER.info("Удаление невозможно, клиента с таким ID нет");
             return "bad_request";
         }
     }
 
     @RequestMapping("/search_by_id")
     public String searchById(int id, Model model) {
-        if (customerService.findById(id).isPresent()) {
+        if (customerService.findById(id).isEmpty()) {
+            LOGGER.info("Поиск по ID не дал результатов. Введен несуществующий ID");
+            return "not_found";
+        } else {
+            LOGGER.info("Поиск по ID выполнен.");
             model.addAttribute("customer", customerService.findById(id).get());
             return "search_by_id";
-        } else {
-            return "not_found";
         }
     }
 
     @RequestMapping("/search_by_fullname")
     public String searchByFullName(String surname, String name, String patronymic, Model model) {
         List<Customer> customers = customerService.findByFullName(surname, name, patronymic);
+        LOGGER.info("Выполнен поиск по ФИО, клиентов найдено:" + customers.size());
         if (customers.isEmpty()) {
             return "not_found";
         }
